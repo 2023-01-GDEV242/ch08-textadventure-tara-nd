bac3e62b0b92965945fedbf89fd2c0045ec3e3df
Tara <63163692+tara-nd@users.noreply.github.com>
2023-04-10 23:44:15 -0400

    added trap door

--- Room.java
+++ Room.java
@@ -26,6 +26,39 @@ import java.util.ArrayList;
 public class Room {
     private String description;
     private ArrayList<Item> items;
+    private Room trapdoorRoom; // the room on the other side of the trap door
+    private boolean trapdoorLocked; // whether the trap door is locked or not
+    
+    // constructor
+    public Room() {
+        trapdoorRoom = null;
+        trapdoorLocked = false;
+    }
+    
+    // sets the room on the other side of the trap door
+    public void setTrapdoorRoom(Room trapdoorRoom) {
+        this.trapdoorRoom = trapdoorRoom;
+    }
+    
+    // locks the trap door
+    public void lockTrapdoor() {
+        trapdoorLocked = true;
+    }
+    
+    // unlocks the trap door
+    public void unlockTrapdoor() {
+        trapdoorLocked = false;
+    }
+    
+    // returns the room on the other side of the trap door
+    public Room getTrapdoorRoom() {
+        return trapdoorRoom;
+    }
+    
+    // checks if the trap door is locked
+    public boolean isTrapdoorLocked() {
+        return trapdoorLocked;
+    }
 
     public Room(String description) {
         this.description = description;
