fda15631802822031aa6caef0b4bdda803cceccf
Tara <63163692+tara-nd@users.noreply.github.com>
2023-04-10 23:30:28 -0400

    implement back command

--- Game.java
+++ Game.java
@@ -1,3 +1,4 @@
+import java.util.Stack;
 /**
  *  This class is the main class of the "World of Zuul" application. 
  *  "World of Zuul" is a very simple, text based adventure game.  Users 
@@ -19,69 +20,54 @@ public class Game
 {
     private Parser parser;
     private Room currentRoom;
-        
-    /**
-     * Create the game and initialise its internal map.
-     */
+    private Stack<Room> previousRooms;
+
     public Game() 
     {
         createRooms();
         parser = new Parser();
+        previousRooms = new Stack<>();
     }
 
-    /**
-     * Create all the rooms and link their exits together.
-     */
     private void createRooms()
     {
         Room outside, theater, pub, lab, office;
-      
+
         // create the rooms
         outside = new Room("outside the main entrance of the university");
         theater = new Room("in a lecture theater");
         pub = new Room("in the campus pub");
         lab = new Room("in a computing lab");
         office = new Room("in the computing admin office");
-        
-        
+
+        // add items to the rooms
         Item key = new Item("key", 0.5);
-        Room lockedRoom = new Room("You are in a locked room.");
-        lockedRoom.setItem(key);
         Item book = new Item("book", 1.0);
-        Item laptop = new Item("laptop",2.0);
-    
-        
+        Item laptop = new Item("laptop", 2.0);
+
+        outside.addItem(key);
+        theater.addItem(book);
+        pub.addItem(laptop);
+
         // initialise room exits
         outside.setExit("east", theater);
         outside.setExit("south", lab);
         outside.setExit("west", pub);
-
         theater.setExit("west", outside);
-
         pub.setExit("east", outside);
-
         lab.setExit("north", outside);
         lab.setExit("east", office);
-
         office.setExit("west", lab);
-        
-        lab.setItem(book);
-        theater.setItem(laptop);
-        office.setItem(key);
 
-        currentRoom = outside;  // start game outside
+        currentRoom = outside;
     }
 
-    /**
-     *  Main play routine.  Loops until end of play.
-     */
     public void play() 
     {            
         printWelcome();
 
         // Enter the main command loop.  Here we repeatedly read commands and
         // execute them until the game is over.
-                
         boolean finished = false;
         while (! finished) {
             Command command = parser.getCommand();
@@ -90,58 +76,40 @@ public class Game
         System.out.println("Thank you for playing.  Good bye.");
     }
 
-    /**
-     * Print out the opening message for the player.
-     */
     private void printWelcome()
     {
         System.out.println();
         System.out.println("Welcome to the World of Zuul!");
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
-        System.out.println("Type '" + CommandWord.HELP + "' if you need help.");
+        System.out.println("Type 'help' if you need help.");
         System.out.println();
         System.out.println(currentRoom.getLongDescription());
     }
 
-    /**
-     * Given a command, process (that is: execute) the command.
-     * @param command The command to be processed.
-     * @return true If the command ends the game, false otherwise.
-     */
     private boolean processCommand(Command command) 
     {
         boolean wantToQuit = false;
 
-        CommandWord commandWord = command.getCommandWord();
-
-        switch (commandWord) {
-            case UNKNOWN:
-                System.out.println("I don't know what you mean...");
-                break;
-
-            case HELP:
-                printHelp();
-                break;
-
-            case GO:
-                goRoom(command);
-                break;
-                
-            case LOOK:
-                look(command);
-                break;
-            
-            case EAT:
-                eat();
-                break;
-           
-
-            case QUIT:
-                wantToQuit = quit(command);
-                break;
+        if(command.isUnknown()) {
+            System.out.println("I don't know what you mean...");
+            return false;
         }
+
+        String commandWord = command.getCommandWord();
+        if (commandWord.equals("help")) {
+            printHelp();
+        }
+        else if (commandWord.equals("go")) {
+            goRoom(command);
+        }
+        else if (commandWord.equals("back")) {
+            back();
+        }
+        else if (commandWord.equals("quit")) {
+            wantToQuit = quit(command);
+        }
+
         return wantToQuit;
-    }
 
     // implementations of user commands:
 
--- Room.java
+++ Room.java
@@ -21,7 +21,7 @@ import java.util.ArrayList;
 
 
 
-import java.util.ArrayList;
+
 
 public class Room {
     private String description;
